3f3d44ea371133e10805341bc195ce013d19742f
Roman <rgk.I@ya.ru>
2020-09-04 13:38:54 +0300

    hton64 refactored

--- libs/support/src/network.cpp
+++ libs/support/src/network.cpp
@@ -2,26 +2,33 @@
 
 #include <arpa/inet.h>
 
-void swap_bytes(uint8_t* lhs, uint8_t* rhs) {
+static void swap_bytes(uint8_t* lhs, uint8_t* rhs) {
   uint8_t tmp = *lhs;
   *lhs = *rhs;
   *rhs = tmp;
 }
 
+static void change_byte_order64(uint64_t* num_ptr) {
+  for (int i = 0; i < 4; ++i) {
+    auto* bytes = reinterpret_cast<uint8_t*>(num_ptr);
+    swap_bytes(bytes + i, bytes + 7 - i);
+  }
+}
+
 uint64_t hton64(uint64_t host64) {
   uint32_t host_one = 1;
-  if (htonl(host_one) == host_one) {
-    return host64;
-  }
-
-  for (int i = 0; i < 4; ++i) {
-    auto* bytes = reinterpret_cast<uint8_t*>(&host64);
-    swap_bytes(bytes + i, bytes + 7 - i);
+  if (htonl(host_one) != host_one) {
+    change_byte_order64(&host64);
   }
 
   return host64;
 }
 
 uint64_t ntoh64(uint64_t net64) {
+  uint32_t net_one = 1;
+  if (ntohl(net_one) != net_one) {
+    change_byte_order64(&net64);
+  }
+
   return net64;
 }
